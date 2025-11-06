307e3eefc032ae0b3920652a1b3056c5120b22a4
simaran sahu <simaransahu4@gmail.com>
2025-11-06 23:39:58 +0530

    update env config for render

--- vite-project/src/component/Home.jsx
+++ vite-project/src/component/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Banner from './Banner'
-import FreeBook from './FreeBook'
+// Filename on disk is `Freebook.jsx` (lowercase 'b').
+// On Windows this resolves case-insensitively, but Linux (Vercel) is case-sensitive.
+// Import using the exact filename so builds on Linux succeed.
+import FreeBook from './Freebook'
 import Footer from './Footer'
 
 function Home() {
